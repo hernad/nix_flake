aa219108b7018be934c69528347208ece789e723
Ernad Husremovic <hernad@bring.out.ba>
2023-03-17 16:33:44 +0100

    en_US

--- traits/base.nix
+++ traits/base.nix
@@ -9,7 +9,7 @@
     # Windows wants hardware clock in local time instead of UTC
     time.hardwareClockInLocalTime = true;
 
-    i18n.defaultLocale = "bs_BA.UTF-8";
+    i18n.defaultLocale = "en_US.UTF-8";
     i18n.supportedLocales = [ "all" ];
 
     environment.systemPackages = with pkgs; [
