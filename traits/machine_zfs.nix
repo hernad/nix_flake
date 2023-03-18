61c3f19510a745d50c25d1b438729fe152098d1c
Ernad Husremovic <hernad@bring.out.ba>
2023-03-18 19:51:27 +0100

    zfs boot

--- traits/machine_zfs.nix
+++ traits/machine_zfs.nix
@@ -48,21 +48,16 @@ in
     boot.loader.grub.copyKernels = true;
     boot.loader.grub.efiSupport = true;
     boot.loader.grub.zfsSupport = true;
-    boot.loader.grub.extraPrepareConfig = ''
-      mkdir -p /boot/efis
-      for i in  /boot/efis/*; do mount $i ; done
-
-      mkdir -p /boot/efi
-      mount /boot/efi
-    '';
-    boot.loader.grub.extraInstallCommands = ''
-      ESP_MIRROR=$(mktemp -d)
-      cp -r /boot/efi/EFI $ESP_MIRROR
-      for i in /boot/efis/*; do
-        cp -r $ESP_MIRROR/EFI $i
-      done
-      rm -rf $ESP_MIRROR
-    '';
+    #boot.loader.grub.extraPrepareConfig = ''
+    #  mkdir -p /boot/efis
+    #  for i in  /boot/efis/*; do mount $i ; done
+    #
+    #  mkdir -p /boot/efi
+    #  mount /boot/efi
+    #'';
+    #boot.loader.grub.extraInstallCommands = ''
+    #  cp -r /boot/efis/${efiPart}/EFI /boot/efis/
+    #'';
     boot.loader.grub.devices = [
       diskTarget
     ];
