c703f46267695c43a294a797fdf1c14719584b1a
Ernad Husremovic <hernad@bring.out.ba>
2023-03-18 10:40:22 +0100

    vscode extensions

--- traits/base.nix
+++ traits/base.nix
@@ -51,6 +51,21 @@
       graphviz
       simple-http-server
       vscode
+      (vscode-with-extensions.override {
+        vscodeExtensions = with vscode-extensions; [
+          bbenoist.nix
+          ms-python.python
+          ms-azuretools.vscode-docker
+          ms-vscode-remote.remote-ssh
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "remote-ssh-edit";
+            publisher = "ms-vscode-remote";
+            version = "0.47.2";
+            sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+          }
+        ];
+      })
     ];
     environment.shellAliases = { };
     environment.variables = {
@@ -97,24 +112,7 @@
     # Hack: https://github.com/NixOS/nixpkgs/issues/180175
     systemd.services.systemd-udevd.restartIfChanged = false;
 
-    environment.systemPackages = with pkgs; [
-      (vscode-with-extensions.override {
-        vscodeExtensions = with vscode-extensions; [
-          bbenoist.nix
-          ms-python.python
-          ms-azuretools.vscode-docker
-          ms-vscode-remote.remote-ssh
-        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-          {
-            name = "remote-ssh-edit";
-            publisher = "ms-vscode-remote";
-            version = "0.47.2";
-            sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-          }
-        ];
-      })
-    ];
-
+    
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
